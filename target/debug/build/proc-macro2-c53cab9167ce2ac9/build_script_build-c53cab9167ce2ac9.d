00bbaea341ddbdee3158dcc22a7323a7eed1d03e
DarkChill42 <borisbodvanski@gmail.com>
2025-08-22 21:23:15 +0200

    Remove .idea and target from repo, add to .gitignore

--- target/debug/build/proc-macro2-c53cab9167ce2ac9/build_script_build-c53cab9167ce2ac9.d
+++
@@ -1,5 +0,0 @@
-C:\Users\boris\OneDrive\Desktop\RustStuff\RustFileOrganizations\target\debug\build\proc-macro2-c53cab9167ce2ac9\build_script_build-c53cab9167ce2ac9.d: C:\Users\boris\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\proc-macro2-1.0.101\build.rs
-
-C:\Users\boris\OneDrive\Desktop\RustStuff\RustFileOrganizations\target\debug\build\proc-macro2-c53cab9167ce2ac9\build_script_build-c53cab9167ce2ac9.exe: C:\Users\boris\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\proc-macro2-1.0.101\build.rs
-
-C:\Users\boris\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\proc-macro2-1.0.101\build.rs:
